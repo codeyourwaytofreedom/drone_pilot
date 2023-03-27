ca600eed044e3aa0ea6970d57fd36fcd7beca1ec
codeyourwaytofreedom <can.kolej@gmail.com>
2023-03-27 21:04:27 +0300

    Npm run build now works.
    Vercel testing.
    
    Keep going.
    
    Remember why you started.

--- components/Drone.tsx
+++ components/Drone.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { SVGLoader } from 'three/examples/jsm/loaders/SVGLoader';
 import { Shape } from "three";
 import { Color } from "three";
@@ -6,29 +7,32 @@ type svg_shape = {
     shape:Shape;
     color:Color;
 }
-let shapes:svg_shape[] =[];
-
-const ur:string = "/propeller.svg";
-
-const loader = new SVGLoader();
-loader.load(ur, function(data){
-    data.paths.map((shp) => shapes.push({shape:SVGLoader.createShapes(shp)[0],color:shp.color }))
-})
-
 
 const extrudeSettings = { depth: 70, bevelEnabled: true, bevelSegments: 9, steps: 2, bevelSize: 1, bevelThickness: 1 };
 
 const Drone = () => {
+    const [shapes, setShapes] = useState<svg_shape[]>([]);
+    
+    useEffect(() => {
+        const loader = new SVGLoader();
+        loader.load("/propeller.svg", function(data){
+            const newShapes = data.paths.map((shp) => ({shape:SVGLoader.createShapes(shp)[0],color:shp.color }));
+            setShapes(newShapes);
+        });
+    }, []);
+    
     return ( 
         <>
-        <mesh>
-            <boxGeometry args={[0.2,0.2,0.2]}/>
-            <meshBasicMaterial color={"black"}/>
-        </mesh> 
-        <mesh scale={0.002}>
-            <extrudeGeometry args={[shapes[0].shape,extrudeSettings]} />
-            <meshBasicMaterial color={"red"}/>
-        </mesh>
+            <mesh>
+                <boxGeometry args={[0.2,0.2,0.2]}/>
+                <meshBasicMaterial color={"black"}/>
+            </mesh> 
+            {shapes.map((s,i) =>
+                <mesh key={i} scale={0.002}>
+                    <extrudeGeometry args={[s.shape,extrudeSettings]} />
+                    <meshBasicMaterial color={s.color}/>
+                </mesh>
+            )}
         </>
      );
 }
