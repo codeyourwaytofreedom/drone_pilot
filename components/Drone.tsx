83d1afdb1217ad519bad3fc113bf50e22d7387dc
codeyourwaytofreedom <can.kolej@gmail.com>
2023-03-28 16:34:30 +0300

    Propeller color design done.
    Propeller motion done.
    
    Next step: Drone body!
    
    Keep going.
    
    Remember why you started.

--- components/Drone.tsx
+++ components/Drone.tsx
@@ -10,8 +10,12 @@ type svg_shape = {
     color:Color;
 }
 
-const extrudeSettings = { depth: 10, bevelEnabled: true, bevelSegments: 9, steps: 2, bevelSize: 1, bevelThickness: 1 };
+const extrudeSettings = { depth: 5, bevelEnabled: true, bevelSegments: 9, steps: 2, bevelSize: 1, bevelThickness: 1 };
 const full_circle = 3.141;
+const rotation_speed = 0.15;
+const propeller_scale = 0.005;
+const propeller_pos = Math.PI/1.7;
+const center_color = "yellow";
 
 const Drone = () => {
     //set the 3D object from the loader
@@ -24,17 +28,26 @@ const Drone = () => {
     }, []);
 
     const [shapes, setShapes] = useState<svg_shape[]>([]);
-    const [excluded,setExcluded] = useState<number[]>([]);
-    const propeller0 = useRef<Group>(null);
+    const [excluded,setExcluded] = useState<number[]>([0,3]);
     const propeller1 = useRef<Group>(null);
     const propeller2 = useRef<Group>(null);
     const propeller3 = useRef<Group>(null);
+    const propeller4 = useRef<Group>(null);
 
-    useFrame(()=>{
-        if(propeller0.current){
-            propeller0.current.rotation.z += 0.01;
+/*     useFrame(()=>{
+        if(propeller1.current){
+            propeller1.current.rotation.z -= rotation_speed;
         }
-    })
+        if(propeller3.current){
+            propeller3.current.rotation.z -= rotation_speed;
+        }
+        if(propeller2.current){
+            propeller2.current.rotation.z += rotation_speed;
+        }
+        if(propeller4.current){
+            propeller4.current.rotation.z += rotation_speed;
+        }
+    }) */
 
     return ( 
         <>
@@ -44,65 +57,67 @@ const Drone = () => {
                 <meshBasicMaterial color={"blue"}/>
             </mesh> 
 
-            <group ref={propeller0} position={[-2,2,0]}>
-                <mesh>
+            <group ref={propeller1} position={[-2,2,0]} rotation={[0,0,propeller_pos]}>
+{/*                 <mesh>
                     <boxGeometry args={[0.2,0.2,0.2]}/>
-                    <meshBasicMaterial color={"green"}/>
-                </mesh> 
+                    <meshBasicMaterial color={"red"}/>
+                </mesh>  */}
                 <group position={[-0.45,-1,0]}>
                     {shapes.map((s,i) => !excluded.includes(i) &&
-                        <mesh key={i} scale={0.005} onClick={()=> setExcluded([...excluded, i])}>
+                        <mesh key={i} scale={propeller_scale} onClick={()=> setExcluded([...excluded, i])}>
                             <extrudeGeometry args={[s.shape,extrudeSettings]} />
-                            <meshBasicMaterial color={s.color}/>
+                            <meshBasicMaterial color={i==4 ? center_color : s.color}/>
                         </mesh>
                     )}
                 </group>
             </group>
 
-            <group ref={propeller1} position={[2,-2,0]}>
-                <mesh>
+            <group ref={propeller3} position={[2,-2,0]} rotation={[0,0,propeller_pos]}>
+{/*                 <mesh>
                     <boxGeometry args={[0.2,0.2,0.2]}/>
-                    <meshBasicMaterial color={"green"}/>
-                </mesh> 
+                    <meshBasicMaterial color={"red"}/>
+                </mesh>  */}
                 <group position={[-0.45,-1,0]}>
                     {shapes.map((s,i) => !excluded.includes(i) &&
-                        <mesh key={i} scale={0.005} onClick={()=> setExcluded([...excluded, i])}>
+                        <mesh key={i} scale={propeller_scale} onClick={()=> setExcluded([...excluded, i])}>
                             <extrudeGeometry args={[s.shape,extrudeSettings]} />
-                            <meshBasicMaterial color={s.color}/>
+                            <meshBasicMaterial color={i==4 ? center_color : s.color}/>
                         </mesh>
                     )}
                 </group>
             </group>
 
-            <group ref={propeller2} position={[-2,-2,0]}>
-                <mesh>
+
+            <group ref={propeller2} position={[2,2,0]} rotation={[0,0,-propeller_pos]}>
+{/*                 <mesh>
                     <boxGeometry args={[0.2,0.2,0.2]}/>
-                    <meshBasicMaterial color={"green"}/>
-                </mesh> 
+                    <meshBasicMaterial color={"yellow"}/>
+                </mesh>  */}
                 <group position={[-0.45,-1,0]}>
                     {shapes.map((s,i) => !excluded.includes(i) &&
-                        <mesh key={i} scale={0.005} onClick={()=> setExcluded([...excluded, i])}>
+                        <mesh key={i} scale={propeller_scale} onClick={()=> setExcluded([...excluded, i])}>
                             <extrudeGeometry args={[s.shape,extrudeSettings]} />
-                            <meshBasicMaterial color={s.color}/>
+                            <meshBasicMaterial color={i==4 ? center_color : s.color}/>
                         </mesh>
                     )}
                 </group>
             </group>
 
-            <group ref={propeller3} position={[2,2,0]}>
-                <mesh>
+            <group ref={propeller4} position={[-2,-2,0]} rotation={[0,0,-propeller_pos]}>
+{/*                 <mesh>
                     <boxGeometry args={[0.2,0.2,0.2]}/>
-                    <meshBasicMaterial color={"green"}/>
-                </mesh> 
+                    <meshBasicMaterial color={"yellow"}/>
+                </mesh>  */}
                 <group position={[-0.45,-1,0]}>
                     {shapes.map((s,i) => !excluded.includes(i) &&
-                        <mesh key={i} scale={0.005} onClick={()=> setExcluded([...excluded, i])}>
+                        <mesh key={i} scale={propeller_scale} onClick={()=> setExcluded([...excluded, i])}>
                             <extrudeGeometry args={[s.shape,extrudeSettings]} />
-                            <meshBasicMaterial color={s.color}/>
+                            <meshBasicMaterial color={i==4 ? center_color : s.color}/>
                         </mesh>
                     )}
                 </group>
             </group>
+
         </>
      );
 }
