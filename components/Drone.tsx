7ce3118e78053a4a0b00cce3f870258bb1aa2155
codeyourwaytofreedom <can.kolej@gmail.com>
2023-03-28 15:31:12 +0300

    Drone propeller in place. Propeller rotation in place.
    Starting to look nice.
    
    Keep going.
    
    Remmeber why you started.

--- components/Drone.tsx
+++ components/Drone.tsx
@@ -1,19 +1,20 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { SVGLoader } from 'three/examples/jsm/loaders/SVGLoader';
-import { Shape } from "three";
+import { Group, InstancedMesh, Shape } from "three";
 import { Color } from "three";
-
+import { useFrame } from '@react-three/fiber';
+import * as THREE from "three";
+import { count } from 'console';
 type svg_shape = {
     shape:Shape;
     color:Color;
 }
 
 const extrudeSettings = { depth: 10, bevelEnabled: true, bevelSegments: 9, steps: 2, bevelSize: 1, bevelThickness: 1 };
+const full_circle = 3.141;
 
 const Drone = () => {
-    const [shapes, setShapes] = useState<svg_shape[]>([]);
-    const [excluded,setExcluded] = useState<number[]>([]);
-
+    //set the 3D object from the loader
     useEffect(() => {
         const loader = new SVGLoader();
         loader.load("/p2.svg", function(data){
@@ -21,19 +22,87 @@ const Drone = () => {
             setShapes(newShapes);
         });
     }, []);
-    
+
+    const [shapes, setShapes] = useState<svg_shape[]>([]);
+    const [excluded,setExcluded] = useState<number[]>([]);
+    const propeller0 = useRef<Group>(null);
+    const propeller1 = useRef<Group>(null);
+    const propeller2 = useRef<Group>(null);
+    const propeller3 = useRef<Group>(null);
+
+    useFrame(()=>{
+        if(propeller0.current){
+            propeller0.current.rotation.z += 0.01;
+        }
+    })
+
     return ( 
         <>
+            {/* center anchor */}
             <mesh>
                 <boxGeometry args={[0.2,0.2,0.2]}/>
-                <meshBasicMaterial color={"black"}/>
+                <meshBasicMaterial color={"blue"}/>
             </mesh> 
-            {shapes.map((s,i) => !excluded.includes(i) &&
-                <mesh key={i} scale={0.005} onClick={()=> setExcluded([...excluded, i])}>
-                    <extrudeGeometry args={[s.shape,extrudeSettings]} />
-                    <meshBasicMaterial color={s.color}/>
-                </mesh>
-            )}
+
+            <group ref={propeller0} position={[-2,2,0]}>
+                <mesh>
+                    <boxGeometry args={[0.2,0.2,0.2]}/>
+                    <meshBasicMaterial color={"green"}/>
+                </mesh> 
+                <group position={[-0.45,-1,0]}>
+                    {shapes.map((s,i) => !excluded.includes(i) &&
+                        <mesh key={i} scale={0.005} onClick={()=> setExcluded([...excluded, i])}>
+                            <extrudeGeometry args={[s.shape,extrudeSettings]} />
+                            <meshBasicMaterial color={s.color}/>
+                        </mesh>
+                    )}
+                </group>
+            </group>
+
+            <group ref={propeller1} position={[2,-2,0]}>
+                <mesh>
+                    <boxGeometry args={[0.2,0.2,0.2]}/>
+                    <meshBasicMaterial color={"green"}/>
+                </mesh> 
+                <group position={[-0.45,-1,0]}>
+                    {shapes.map((s,i) => !excluded.includes(i) &&
+                        <mesh key={i} scale={0.005} onClick={()=> setExcluded([...excluded, i])}>
+                            <extrudeGeometry args={[s.shape,extrudeSettings]} />
+                            <meshBasicMaterial color={s.color}/>
+                        </mesh>
+                    )}
+                </group>
+            </group>
+
+            <group ref={propeller2} position={[-2,-2,0]}>
+                <mesh>
+                    <boxGeometry args={[0.2,0.2,0.2]}/>
+                    <meshBasicMaterial color={"green"}/>
+                </mesh> 
+                <group position={[-0.45,-1,0]}>
+                    {shapes.map((s,i) => !excluded.includes(i) &&
+                        <mesh key={i} scale={0.005} onClick={()=> setExcluded([...excluded, i])}>
+                            <extrudeGeometry args={[s.shape,extrudeSettings]} />
+                            <meshBasicMaterial color={s.color}/>
+                        </mesh>
+                    )}
+                </group>
+            </group>
+
+            <group ref={propeller3} position={[2,2,0]}>
+                <mesh>
+                    <boxGeometry args={[0.2,0.2,0.2]}/>
+                    <meshBasicMaterial color={"green"}/>
+                </mesh> 
+                <group position={[-0.45,-1,0]}>
+                    {shapes.map((s,i) => !excluded.includes(i) &&
+                        <mesh key={i} scale={0.005} onClick={()=> setExcluded([...excluded, i])}>
+                            <extrudeGeometry args={[s.shape,extrudeSettings]} />
+                            <meshBasicMaterial color={s.color}/>
+                        </mesh>
+                    )}
+                </group>
+            </group>
         </>
      );
 }
