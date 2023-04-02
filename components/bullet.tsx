5e0cc52820d809c9ec6a65cd6a4b779e0b004cd1
codeyourwaytofreedom <can.kolej@gmail.com>
2023-04-02 13:44:54 +0300

    Bullet takes the path of drone rotation.
    Good start today.
    
    Will preserve the rot after firing.
    
    Keep going.
    
    Remember why you started.

--- components/bullet.tsx
+++ components/bullet.tsx
@@ -34,8 +34,8 @@ const Bullet:NextPage<rot_mov> = ({drone_positionX, drone_positionY,drone_rotati
     },[])
     useFrame(()=>{
         if(bullet.current && triggered){    
-            const speed = 0.1; // adjust this to control the speed of movement
-            const direction = new THREE.Vector3(0, 1, 0); // initial direction vector
+            const speed = 0.05; // adjust this to control the speed of movement
+            const direction = new THREE.Vector3(0, -drone_rotationUD, 0); // initial direction vector 
             const rotation = bullet.current.rotation.clone(); // get the object's rotation
             direction.applyEuler(rotation); // rotate the direction vector to match the object's rotation
             const delta = direction.multiplyScalar(speed); // calculate the movement delta
@@ -44,7 +44,7 @@ const Bullet:NextPage<rot_mov> = ({drone_positionX, drone_positionY,drone_rotati
         }
     })
     return ( 
-        <group position={[0,0,0]} ref={bullet} scale={0.4} rotation={[0,0,-1]}>
+        <group position={[drone_positionX,drone_positionY,0]} ref={bullet} scale={0.4} rotation={[drone_rotationUD,0,0]}>
         <mesh>
             <cylinderGeometry args={[0.2,0.2,2]}/>
             <meshBasicMaterial color={"red"}/>
